e681b87fbb5a6e7e44e9d722c35f7975d34e1e19
thomas bunting <bunting.tommy@gmail.com>
2024-07-03 07:38:39 +0100

    Add project files.

---
+++ CommunityEventPlanner.api.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CommunityEventPlanner.api.IntegrationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
